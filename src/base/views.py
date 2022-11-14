6a363a4179648aa47a7825d469ee44b2d4f32aa0
Jorge Osses <jorgitoosses@gmail.com>
2022-11-14 01:59:03 -0300

    Bootstrap styles

--- src/base/views.py
+++ src/base/views.py
@@ -9,10 +9,13 @@ from django.views.generic.base import TemplateView
 class HomePageView(TemplateView):
     template_name = 'base/home.html'
 
-    def home(self, request, *args, **kwargs):
-        return render(request, self.template_name, {
-            'title': 'Match'
-        })
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Match"
+        return context
+        # return render(request, self.template_name, {
+        #     'title': 'Match'
+        # })
 
 
 class FaqView(TemplateView):

2d67e7e387e3fe2a0fa670f79af1ec8cbda98495
Project Django <jorgitoosses@gmail.com>
2022-12-19 20:30:20 -0300

    version 1.4

--- match/settings.py
+++ match/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from .mailjet import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p^1w6a!_h*h!fdc4w5t_ary%t(0k(+g(&v8##&z2%)9lww5*9)'
+SECRET_KEY = SECRET_KEY
 # SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -137,7 +137,7 @@ LOGIN_REDIRECT_URL = 'list'
 
 LOGOUT_REDIRECT_URL = 'home'
 
-from .mailjet import *
+
 
 #emails
 if DEBUG:
